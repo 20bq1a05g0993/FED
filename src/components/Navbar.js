26fe29a7642107a90c8a80bad72de09081b3acfc
20bq1a05g0993 <nani17200314@gmail.com>
2022-10-11 21:33:15 +0530

    fourth commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,22 +5,22 @@ const Navbar = () => {
     <div className="nav-menu d-flex flex-row justify-content-center">
       <div className="logo">
           <a href="">
-            <img src="https://www.campusoption.com/images/colleges/logos/13_02_17_063222_VV_LL.gif" alt="VVIT LOGO" height="100px" width="100px" />
+            <img src="https://www.campusoption.com/images/colleges/logos/13_02_17_063222_VV_LL.gif" alt="VVIT LOGO" height="100px" width="100px"  />
           </a>
       </div>
       <div className="menu">
         <ul>
           <li>
-            <a href="#home">HOME</a>
+            <a href="#home"></a>
           </li>
           <li>
-            <a href="#about">ABOUT</a>
+            <a href="#about"></a>
           </li>
           <li>
-            <a href="#work">PORTFOLIO</a>
+            <a href="#work"></a>
           </li>
           <li>
-            <a href="#clients">CONTACT</a>
+            <a href="#clients"></a>
           </li>
         </ul>
       </div>
