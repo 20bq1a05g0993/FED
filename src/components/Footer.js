9b55d28e4d1289a1d38b35d29266e5a267ee7f28
20bq1a05g0993 <nani17200314@gmail.com>
2022-10-11 20:24:39 +0530

    second commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <div>
       <div className="footer">
         <div className="footer-content">
-            <h3>My FED Blog</h3>
+            <h3>My FED </h3>
             <p>copyright &copy;2021 <a href="#"> My FED Blog</a>  </p>
         </div>
       </div>
